e4e10ccf9caf7124cc8d54555c4b2b9ee2624517
bruna carlota <bru.carlota@yahoo.com.br>
2021-12-03 09:13:00 -0400

    add some style and organize stuff

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,13 @@
 const url = '../data.json';
 
 let html = '';
-let main = document.querySelector('#main');
+let main = document.querySelector('#time-tracking');
 
 fetch(url, { mode: "no-cors" })
   .then((res) => res.json())
   .then((itens) => {
     console.log( typeof(itens), itens);
-    html += `<div>`;
+    // html += `<div>`;
     
     for( item of itens ){
       html += `<div class='card ${item.title}'>
@@ -16,16 +16,25 @@ fetch(url, { mode: "no-cors" })
       console.log(item.title);
       const entries = Object.entries(item.timeframes);
       entries.forEach( x => {
-        html += `<div class='${x[0]}'>
+        html += `<div class='timeframe ${x[0]}'>
                   <h3 class='current'>${x[1]['current']}</h3>
                   <span class='previous'>${x[1]['previous']}</span>
                  </div>`;
       });
-      html += `</div>
-            </div>`;
+      html += `</div>`;
     }
     main.innerHTML = html;
   });
 
 
-
+const selectOption = (param) => {
+  const options = document.querySelectorAll('.timeframe');
+  let selected = document.querySelectorAll(`.${param.innerHTML}`);
+  
+  options.forEach( e => e.classList.remove('visible'));
+  selected.forEach(
+    e => e.classList.add('visible')
+  )
+  
+  
+}

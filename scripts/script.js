ddf4f9b3d34dc3aa8800fd41331eff49d65b75c8
bruna carlota <bru.carlota@yahoo.com.br>
2021-12-06 13:40:56 -0400

    style cards and responsive

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,24 @@ fetch(url, { mode: "no-cors" })
     
     for( item of itens ){
       const className = item.title.replace(' ', '-').toLowerCase();
-      
-      html += `<div class='item-wrap ${className}'>
+
+      html += `<div class='item-wrap ${className}' onclick='activeTimeframe(this)'>
                 <div class='item'>
-                  <h4 class='title'>${item.title}</h4>`;
+                  <div class='title'>
+                    <h4>${item.title}</h4>
+                    <span class='ellipsis'>...</span>
+                  </div>`;
+      
       const entries = Object.entries(item.timeframes);
-      entries.forEach( x => {
-        html += `<div class='timeframe ${x[0]}'>
-                  <h3 class='current'>${x[1]['current']}</h3>
-                  <span class='previous'>${x[1]['previous']}</span>
+      entries.forEach(data => {
+        
+        const opt = data[0] == 'weekly' ? 'Week' : 'Month';
+        const previous = data[0] == 'daily' ? `Yesterday - ${data[1]['previous']}hrs` 
+                                            : `Last ${opt} - ${data[1]['previous']}hrs`
+
+        html += `<div class='timeframe ${data[0]}'>
+                  <h3 class='current'>${data[1]['current']}hrs</h3>
+                  <span class='previous'>${previous}</span>
                  </div>`;
       });
       html +=   `</div>
@@ -29,17 +38,23 @@ fetch(url, { mode: "no-cors" })
   });
 
 
-const selectOption = (param) => {
+const activeOption = (elem) => {
   const optionsList = document.querySelectorAll('.option ul li');
   optionsList.forEach(e => e.classList.remove('active'));
-  param.classList.add('active');
+  elem.classList.add('active');
 
   const options = document.querySelectorAll('.timeframe');
-  const selected = document.querySelectorAll(`.${param.innerHTML}`);
+  const selected = document.querySelectorAll(`.${elem.innerHTML}`);
   options.forEach( e => e.classList.remove('visible'));
   selected.forEach(
     e => e.classList.add('visible')
   )
-  
-  
+}
+
+const activeTimeframe = (elem) => {
+  const itemList = document.querySelectorAll('.item-wrap');
+  itemList.forEach(e => e.classList.remove('active'));
+  elem.classList.add('active');
+
+
 }
